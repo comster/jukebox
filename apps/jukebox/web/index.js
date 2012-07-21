a5e5e9e7ff2d60902605c6b2cdccfde4614e2b92
Jeff Pelton <j@jeffpelton.com>
2012-07-21 15:26:44 -0700

    dancer test

--- apps/jukebox/web/index.js
+++ apps/jukebox/web/index.js
@@ -35,6 +35,34 @@
                             });
                             
                             require(['dancer.js'], function() {
+                                Dancer.addPlugin( 'waveform', function( canvasEl, options ) {
+                                  options = options || {};
+                                  var
+                                    ctx     = canvasEl.getContext( '2d' ),
+                                    h       = canvasEl.height,
+                                    w       = canvasEl.width,
+                                    width   = options.width || ( Dancer.isSupported() === 'flash' ? 2 : 1 ),
+                                    spacing = options.spacing || 0,
+                                    count   = options.count || 1024;
+                                
+                                  ctx.lineWidth   = options.strokeWidth || 1;
+                                  ctx.strokeStyle = options.strokeStyle || "white";
+                                
+                                  this.bind( 'update', function() {
+                                    var waveform = this.getWaveform();
+                                    ctx.clearRect( 0, 0, w, h );
+                                    ctx.beginPath();
+                                    ctx.moveTo( 0, h / 2 );
+                                    for ( var i = 0, l = waveform.length; i < l && i < count; i++ ) {
+                                      ctx.lineTo( i * ( spacing + width ), ( h / 2 ) + waveform[ i ] * ( h / 2 ));
+                                    }
+                                    ctx.stroke();
+                                    ctx.closePath();
+                                  });
+                                
+                                  return this;
+                                });
+                                
                                 var dancer = new Dancer( "/api/files/02%20Dashboard.mp3" ),
                                 beat = dancer.createBeat({
                                   onBeat: function ( mag ) {
@@ -59,6 +87,10 @@
                                   // After 120s, we'll turn the beat off as another object's y position is still being mapped from the previous "after" method
                                   beat.off();
                                 });
+                                $('body').append('<canvas id="waveform" />');
+                                
+                                var canvas = document.getElementById('waveform');
+                                dancer.waveform( canvas, { strokeStyle: '#ff0077' });
                                 
                                 dancer.play();
                             });
