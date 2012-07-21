5d0706f7b6e3b88a9731030d89d81e767058a2b8
Jeff Pelton <j@jeffpelton.com>
2012-07-21 15:21:51 -0700

    audio tests

--- apps/jukebox/web/index.js
+++ apps/jukebox/web/index.js
@@ -33,6 +33,35 @@
                                     }
                                 });
                             });
+                            
+                            require(['dancer.js'], function() {
+                                var dancer = new Dancer( "/api/files/02%20Dashboard.mp3" ),
+                                beat = dancer.createBeat({
+                                  onBeat: function ( mag ) {
+                                    console.log('Beat!');
+                                  },
+                                  offBeat: function ( mag ) {
+                                    console.log('no beat :(');
+                                  }
+                                });
+                                
+                                beat.on();
+                                
+                                dancer.onceAt( 10, function() {
+                                  // Let's set up some things once at 10 seconds
+                                }).between( 10, 60, function() {
+                                  // After 10s, let's do something on every frame for the first minute
+                                }).after( 60, function() {
+                                  // After 60s, let's get this real and map a frequency to an object's y position
+                                  // Note that the instance of dancer is bound to "this"
+                                  object.y = this.getFrequency( 400 );
+                                }).onceAt( 120, function() {
+                                  // After 120s, we'll turn the beat off as another object's y position is still being mapped from the previous "after" method
+                                  beat.off();
+                                });
+                                
+                                dancer.play();
+                            });
                             if(callback) callback(chat, jukebox);
                         });
                     });
