4a588038074fa2cbe66b7bc95a26b311658cc6da
Jeff Pelton <j@jeffpelton.com>
2012-07-22 02:28:14 -0700

    env port for heroku

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var housejs = require('house');
 
 var config = require(__dirname+'/config/config.js').config;
+config.webPort = process.env.PORT || config.webPort;
+var house = new housejs(config);
 
-    var house = new housejs(config);
+house.useApps(__dirname+'/apps');
 
-    house.useApps(__dirname+'/apps');
-
-    house.start();
+house.start();
 
