ecf8dda56b2587863df5010160b6092ae44dedde
Jeff Pelton <j@jeffpelton.com>
2012-08-27 02:05:41 -0700

    docs

--- web.js
+++
@@ -1,9 +0,0 @@
-var housejs = require('house');
-
-var config = require(__dirname+'/config/config.js').config;
-config.webPort = process.env.PORT || config.webPort;
-var house = new housejs(config);
-
-house.useApps(__dirname+'/apps');
-
-house.start();
